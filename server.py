c04e41c71de3846b4c4959f9f9c7e499520004fa
Mouse Reeve <mouse.reeve@gmail.com>
2015-09-04 16:56:03 -0700

    Adds api endpoint for flower names
    Works on #8

--- server.py
+++ server.py
@@ -1,14 +1,23 @@
 ''' Simple webserver and API routing '''
 from flask import Flask, make_response
+from nominaflora.NominaFlora import NominaFlora
 
 # CONFIG
 app = Flask(__name__)
+flora = NominaFlora()
 
 # ROUTES
 @app.route('/')
 def index():
     ''' Render the basic template for angular '''
     return make_response(open('index.html').read())
+
+# the fun stuff API
+@app.route('/api/flora', methods=['GET'])
+def flower_names():
+    ''' returns a made up flower name '''
+    name = '%s (%s)' % (flora.get_common_name(), flora.get_scientific_name())
+    return name
 
 if __name__ == '__main__':
     app.debug = True
