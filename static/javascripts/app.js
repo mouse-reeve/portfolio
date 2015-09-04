33753b4aca8eef22c3957d8f8288b12724e1d4d6
Mouse Reeve <mouse.reeve@gmail.com>
2015-09-04 13:50:49 -0700

    Adds page titles

--- static/javascripts/app.js
+++ static/javascripts/app.js
@@ -27,34 +27,49 @@ angular.module('app', [
 
         $routeProvider
             .when('/', {
+                title: 'Portfolio',
                 controller: 'MainCtrl',
                 templateUrl: 'static/partials/main.html'
             })
             .when('/clocks', {
+                title: 'Clocks',
                 controller: 'ClocksCtrl',
                 templateUrl: 'static/partials/clocks.html'
             })
             .when('/chomsky', {
+                title: 'Chomsky',
                 templateUrl: 'static/partials/chomsky.html'
             })
             .when('/thesaurus', {
+                title: 'Sense',
                 templateUrl: 'static/partials/thesaurus.html'
             })
             .when('/kipple', {
+                title: 'Kipple',
                 templateUrl: 'static/partials/kipple.html'
             })
             .when('/flora', {
+                title: 'Flora',
                 controller: 'FloraCtrl',
                 templateUrl: 'static/partials/flora.html'
             })
             .when('/resume', {
+                title: 'Resume',
                 controller: 'ResumeCtrl',
                 templateUrl: 'static/partials/resume.html'
             })
             .when('/about', {
+                title: 'About',
                 templateUrl: 'static/partials/about.html'
             })
             .otherwise({
                 redirectTo: '/'
-            });
-    });
+            })
+        ;
+    })
+    .run(['$rootScope', '$route', function($rootScope, $route) {
+        $rootScope.$on('$routeChangeSuccess', function() {
+            document.title = $route.current.title + ' : Mouse Reeve';
+        });
+    }])
+;
