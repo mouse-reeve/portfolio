789ab5c05b020faf9a414af633dc15ba01561b29
Mouse Reeve <mouse.reeve@gmail.com>
2015-09-30 10:54:19 -0700

    Adds github activity count

--- server.py
+++ server.py
@@ -2,6 +2,7 @@
 from flask import Flask, make_response
 from nominaflora.NominaFlora import NominaFlora
 import json
+import urllib2
 
 # CONFIG
 app = Flask(__name__)
@@ -24,6 +25,21 @@ def flower_names():
     ''' returns a made up flower name '''
     name = {'common': flora.get_common_name(), 'scientific': flora.get_scientific_name()}
     return json.dumps(name)
+
+@app.route('/api/activity', methods=['GET'])
+def get_activity():
+    ''' load my activity for the day '''
+    activity = {'all': 0}
+
+    # GitHub activity
+    github = urllib2.urlopen('https://api.github.com/users/mouse-reeve/events/public')
+    github = json.loads(github.read())
+    counts = [item['payload']['size'] if 'size' in item['payload'] else 1 \
+              for item in github if item['created_at'][:10] == '2015-09-29']
+    activity['github'] = reduce(lambda x, y: x+y, counts)
+    activity['all'] += activity['github']
+
+    return json.dumps(activity)
 
 if __name__ == '__main__':
     app.debug = True
