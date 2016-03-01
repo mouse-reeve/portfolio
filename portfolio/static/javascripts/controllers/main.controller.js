103f539ca97ef834b6a96e1646a8cbc965d8d6f4
Mouse Reeve <mousereeve@riseup.net>
2016-02-29 16:04:37 -0800

    Better internal navigation
    Also killed the dead blog link

--- portfolio/static/javascripts/controllers/main.controller.js
+++ portfolio/static/javascripts/controllers/main.controller.js
@@ -2,7 +2,16 @@ angular.module('app').controller('MainCtrl', [
         '$location', '$scope', 'Activity', function($location, $scope, Activity) {
 
     $scope.text = 'it\'s ok to be confused';
-    $scope.pages = ['clocks', 'chomsky', 'thesaurus', 'flora', 'activity', 'resume', 'about'];
+    $scope.pages = [
+        {link: 'clocks', display: 'clocks'},
+        {link: 'chomsky', display: 'chomp'},
+        {link: 'thesaurus', display: 'sense'},
+        {link: 'flora', display: 'flora'},
+        {link: 'kipple', display: 'kipple'},
+        {link: 'activity', display: 'activity'},
+        {link: 'resume', display: 'résumé'},
+        {link: 'about', display: 'about'}
+    ];
 
     Activity.getActivity().then(function (data) {
         $scope.activity = data;
@@ -10,6 +19,6 @@ angular.module('app').controller('MainCtrl', [
 
     $scope.randomPage = function () {
         var page = Math.floor(Math.random() * $scope.pages.length);
-        $location.path($scope.pages[page]);
+        $location.path($scope.pages[page].link);
     };
 }]);
